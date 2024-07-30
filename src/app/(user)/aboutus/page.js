6f56876ed52de1b27b3ec3d445dab4c0e08ea772
ashish0123 <ashish.vnd@quytech.net>
2024-07-30 17:02:22 +0530

    fix clients

--- src/app/(user)/aboutus/page.js
+++ src/app/(user)/aboutus/page.js
@@ -19,6 +19,29 @@ export default function aboutUs() {
         speed: 500,
         slidesToShow: 6,
         slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 1008,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            },
+          },
+        ],
       };
 
       useEffect(() => {
--- src/app/page.js
+++ src/app/page.js
@@ -36,6 +36,29 @@ export default function Home() {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   useEffect(() => {
     initFlowbite(); // Call initCarousels() when component mounts
